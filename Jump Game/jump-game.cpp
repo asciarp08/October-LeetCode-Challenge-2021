1540b6ebeb04be9f666a98ab44affa0e32f23921
Anjali R Pahal <39112406+asciarp08@users.noreply.github.com>
2021-10-03 11:59:07 +0530

    Create jump-game.cpp

---
+++ Jump Game/jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        
+        int maxReach = 0;
+        
+        for(int i = 0; i<=maxReach; i++){
+            maxReach = max(maxReach, i+nums[i]);
+            if(maxReach>=n-1)return true;
+        }
+        return false;
+    }
+};
